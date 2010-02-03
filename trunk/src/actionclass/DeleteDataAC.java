afeb178912f424a52a95713580ecb882bc322e11
justinjose12002 <justinjose12002@2cc1b806-025c-11df-9adb-2bc960bfb704>
2010-02-03 10:46:54 +0000

    added post and pre BL

--- trunk/src/actionclass/DeleteDataAC.java
+++ trunk/src/actionclass/DeleteDataAC.java
@@ -4,15 +4,21 @@ import java.io.InputStream;
 import java.io.StringBufferInputStream;
 import java.net.URLDecoder;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.interceptor.ServletRequestAware;
 
+import businesslogic.BaseBL;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 import crud.DeleteData;
+import dao.CrudDAO;
 
 
 public class DeleteDataAC extends ActionSupport implements ServletRequestAware {
@@ -21,6 +27,7 @@ public class DeleteDataAC extends ActionSupport implements ServletRequestAware {
 	private String screenName;
 	private String whereclause;
 	private HttpServletRequest servletRequest;
+	private HashMap retBLhm = null;
 	
 	private void debug(int priority, String s){
 		if(priority > 1){
@@ -61,8 +68,7 @@ public class DeleteDataAC extends ActionSupport implements ServletRequestAware {
     	whereclause = decoder.decode(request1.getParameter("wclause"));
     	
     	
-    	
-    	
+    	postDeleteProcessBL(screenName);
     	debug(0,"Screen Name  = " +screenName);
     	debug(0,"where clause  = " +whereclause);
     	
@@ -81,4 +87,36 @@ public class DeleteDataAC extends ActionSupport implements ServletRequestAware {
         return SUCCESS;
 	}
 
+	private void postDeleteProcessBL(String screenName2) {
+		
+		Class aclass = null;
+		CrudDAO cd = new CrudDAO();
+		String businessLogic = cd.getBusinessLogicName(screenName);
+		try {
+			if (businessLogic != null && !"".equals(businessLogic)) {
+				aclass = Class.forName(businessLogic);
+				BaseBL basebl = (BaseBL) aclass.newInstance();
+				Map buslogHm = new HashMap();
+
+				Map map = servletRequest.getParameterMap();//parameters;
+				
+				Iterator iter = map.entrySet().iterator();
+				while (iter.hasNext()) {
+					Entry n = (Entry) iter.next();
+					String key = n.getKey().toString();
+					String values[] = (String[]) n.getValue();
+					buslogHm.put(key, values);
+				}	
+				//buslogHm = map;
+				retBLhm = basebl.postDeleteProcessBL(buslogHm);
+			}
+			else{
+				retBLhm.put("error", "Method not found");
+			}
+		} catch (Exception e) {
+			debug(1,"Businesslogic not found");
+			e.printStackTrace();
+		}		
+	}
+		
 }
