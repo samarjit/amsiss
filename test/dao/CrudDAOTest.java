df40bf967d7de630169539a2b57673731805efbb
samarjit.samanta <samarjit.samanta@2cc1b806-025c-11df-9adb-2bc960bfb704>
2010-03-04 09:45:29 +0000

    Test Classes - 9 major classes for reasonable test coverage

---
+++ test/dao/CrudDAOTest.java
@@ -0,0 +1,94 @@
+package dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CrudDAOTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testFindSplWhereClsOfPanels() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindPanelByScrname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindTableByPanels() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateRetrieveQueryPart1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateWhereClause() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExecuteRetrieveQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExecuteInsertQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindPreDefQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateUpdateQueryPart1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateInsertQueryPart1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBusinessLogicName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNewAppId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStoreFlag() {
+		fail("Not yet implemented");
+	}
+
+}
